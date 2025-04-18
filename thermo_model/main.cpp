3540bbd00bf72febb3abc5092dbc03e5c8aa4c67
dward239 <dylanward188@gmail.com>
2025-04-18 17:04:10 -0400

    Update main.cpp

--- thermo_model/main.cpp
+++ thermo_model/main.cpp
@@ -1,1 +1,27 @@
-//placeholder
+#include <iostream>
+#include "species.h"
+#include "pitzer.h"
+#include "solver.h"
+#include "speciation.h"
+
+int main () {
+  std::vector<AqueousSpeies> species = loadspecies("database.txt");
+
+for (auto& sp : species) {
+  if (sp.name == "UO2++" || sp.name == "F-") {
+  sp.isPrimary = true;
+  }
+}
+
+Solution solution;
+TotalInput input;
+input.totals["U"] = 1e-3;
+input.totals["F"] = 2e-3;
+
+solution.concentrations["UO2++"] = 5e-4;
+solutions.concentrations"F-" = 1e-3;
+
+calculateActivityCoefficients(species, solution);
+solveSpeciation(species, solution, input);
+
+return 0;
